33301c01f9cc86630537b6186e0e9c6991c6ccda
devin <wzh.devin@gmail.com>
2026-01-14 12:11:17 +0800

    fix(Authorization): 修复Admin模块鉴权失效问题

--- dezhi-common/src/main/java/com/devin/dezhi/interceptor/SaTokenInterceptor.java
+++ dezhi-common/src/main/java/com/devin/dezhi/interceptor/SaTokenInterceptor.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
  * 2026/1/1 02:09.
